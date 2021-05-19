8a3831448297bfc3bbfc07485f7d70365d9ec474
VitaliyOleinik <oleynik624@gmail.com>
2021-05-19 12:32:30 +0600

    Inversion of Controll

---
+++ spring_course/src/main/java/spring_intro/Test1.java
@@ -0,0 +1,10 @@
+package spring_intro;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Pet dog = new Dog();
+        Pet cat = new Cat();
+        cat.say();
+        dog.say();
+    }
+}
