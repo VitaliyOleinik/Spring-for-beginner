30e14c1418fc4a8d3822b875e5d409c792a4def4
VitaliyOleinik <oleynik624@gmail.com>
2021-05-19 13:27:29 +0600

    DI + set

--- spring_course/src/main/java/spring_intro/Person.java
+++ spring_course/src/main/java/spring_intro/Person.java
@@ -3,13 +3,22 @@ package spring_intro;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet){
+//    public Person(Pet pet){
+//        System.out.println("Person bean is created");
+//        this.pet = pet;
+//    }
+
+    public Person(){
         System.out.println("Person bean is created");
-        this.pet = pet;
     }
 
     public void callYourPet(){
         System.out.println("Hello");
         pet.say();
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set pet");
+        this.pet = pet;
     }
 }
