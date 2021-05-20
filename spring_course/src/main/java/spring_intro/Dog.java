5fcea007313eb93115f78cd17d9a9d8c4a125f0d
VitaliyOleinik <oleynik624@gmail.com>
2021-05-20 18:06:45 +0600

    work with scope Singleton and prototype

--- spring_course/src/main/java/spring_intro/Dog.java
+++ spring_course/src/main/java/spring_intro/Dog.java
@@ -1,9 +1,19 @@
 package spring_intro;
 
 public class Dog implements Pet{
+    private String name;
     Dog(){
         System.out.println("Dog bean is created");
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public void say(){
         System.out.println("Bow-Wow");
---
+++ spring_course/src/main/java/spring_intro/Test5.java
@@ -0,0 +1,17 @@
+package spring_intro;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test5 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext3.xml");
+        Dog myDog = context.getBean("myPet", Dog.class);
+        Dog yourDog = context.getBean("myPet", Dog.class);
+        System.out.println(myDog == yourDog);
+        myDog.setName("Belka");
+        yourDog.setName("Strelka");
+        System.out.println();
+        context.close();
+    }
+}
