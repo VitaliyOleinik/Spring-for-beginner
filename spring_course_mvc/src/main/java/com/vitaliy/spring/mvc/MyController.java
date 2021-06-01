f9abf7fe86550012ccbdc68f2ad07dbfef0fabd7
VitaliyOleinik <oleynik624@gmail.com>
2021-06-01 17:55:10 +0600

    @RequestMapping for class

--- spring_course_mvc/src/main/java/com/vitaliy/spring/mvc/MyController.java
+++ spring_course_mvc/src/main/java/com/vitaliy/spring/mvc/MyController.java
@@ -3,10 +3,10 @@ package com.vitaliy.spring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/employee")
 public class MyController {
     @RequestMapping("/")
     public String showMyView(){
@@ -18,17 +18,24 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
+    @RequestMapping("/showDetails")
+    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model){
+        empName = "Mr. " + empName;
+        model.addAttribute("nameAttribute", empName);
+        return "show-emp-details-view";
+    }
+
 //    @RequestMapping("/showDetails")
 //    public String showEmpDetails(){
 //        return "show-emp-details-view";
 //    }
 
-    @RequestMapping("/showDetails")
-    public String showEmpDetails(HttpServletRequest request, Model model){
-        String empName = request.getParameter("employeeName");
-        empName = "Mr. " + empName;
-        model.addAttribute("nameAttribute", empName);
-        model.addAttribute("description", "best user");
-        return "show-emp-details-view";
-    }
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(HttpServletRequest request, Model model){
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr. " + empName;
+//        model.addAttribute("nameAttribute", empName);
+//        model.addAttribute("description", "best user");
+//        return "show-emp-details-view";
+//    }
 }
