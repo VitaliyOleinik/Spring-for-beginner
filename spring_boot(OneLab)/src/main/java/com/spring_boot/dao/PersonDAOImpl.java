02a44195b4e8524b9df2ff5932a922dc36e5f2c5
VitaliyOleinik <oleynik624@gmail.com>
2021-06-07 22:04:12 +0600

    personDAOImpl taking info from db

--- spring_boot(OneLab)/src/main/java/com/spring_boot/dao/PersonDAOImpl.java
+++ spring_boot(OneLab)/src/main/java/com/spring_boot/dao/PersonDAOImpl.java
@@ -1,27 +1,42 @@
 package com.spring_boot.dao;
 
 import com.spring_boot.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
+@Repository
 public class PersonDAOImpl implements PersonDAO{
+
+    @Autowired
+    EntityManager entityManager;
+
     @Override
     public List<Person> getAllPersons() {
-        return null;
+        Query query = entityManager.createQuery("from Person");
+        List<Person> allPersons = query.getResultList();
+        return allPersons;
     }
 
     @Override
     public void savePerson(Person person) {
-
+        Person newPerson = entityManager.merge(person);
+        person.setId(newPerson.getId());
     }
 
     @Override
     public Person getPerson(int id) {
-        return null;
+        Person person = entityManager.find(Person.class, id);
+        return person;
     }
 
     @Override
     public void deletePerson(int id) {
-
+        Query query = entityManager.createQuery("delete from Person where id =: personId");
+        query.setParameter("personId", id);
+        query.executeUpdate();
     }
 }
