00292ccdaf5a4330548cbed44269009eb1f14503
VitaliyOleinik <oleynik624@gmail.com>
2021-06-14 21:43:00 +0600

    entities + application config for connection with postgre sql

---
+++ spring_boot_postgre/src/test/java/com/spring_boot/spring_boot_postgre/SpringBootPostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot.spring_boot_postgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPostgreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
