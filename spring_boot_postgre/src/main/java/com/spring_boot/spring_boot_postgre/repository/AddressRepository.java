8a8a1cc92788ba8dcbbef59aae89eb007db3205b
VitaliyOleinik <oleynik624@gmail.com>
2021-06-15 22:24:38 +0600

    added JpaReposity

---
+++ spring_boot_postgre/src/main/java/com/spring_boot/spring_boot_postgre/repository/AddressRepository.java
@@ -0,0 +1,6 @@
+package com.spring_boot.spring_boot_postgre.repository;
+
+import com.spring_boot.spring_boot_postgre.entity.Address;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
