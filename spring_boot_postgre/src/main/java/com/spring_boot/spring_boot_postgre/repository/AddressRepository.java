0815e05e4ee12c18f62213b9b7414efcd2f2c5ae
VitaliyOleinik <oleynik624@gmail.com>
2021-06-16 20:44:27 +0600

    added users repository

--- spring_boot_postgre/src/main/java/com/spring_boot/spring_boot_postgre/repository/AddressRepository.java
+++ spring_boot_postgre/src/main/java/com/spring_boot/spring_boot_postgre/repository/AddressRepository.java
@@ -1,6 +1,7 @@
 package com.spring_boot.spring_boot_postgre.repository;
 
 import com.spring_boot.spring_boot_postgre.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }
