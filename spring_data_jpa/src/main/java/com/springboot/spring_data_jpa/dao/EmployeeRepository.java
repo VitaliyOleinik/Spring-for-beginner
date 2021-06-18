b058537003c2da5c128539900175f1c0fa4a3e41
VitaliyOleinik <oleynik624@gmail.com>
2021-06-18 22:34:22 +0600

    added user repository

--- spring_data_jpa/src/main/java/com/springboot/spring_data_jpa/dao/EmployeeRepository.java
+++ spring_data_jpa/src/main/java/com/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -2,9 +2,10 @@ package com.springboot.spring_data_jpa.dao;
 
 import com.springboot.spring_data_jpa.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     List<Employee> findAllByName(String name);
 }
